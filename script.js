c52a226e3c23c454016fe33f5c88cec7540863dc
Dipen Khadka <dipenkhadka191@gmail.com>
2023-04-10 23:56:28 +0545

    -- Final

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const mybutton = document.querySelector("button");
-let quote = document.querySelector(".center p")
+let quote = document.querySelector(".center p");
+let centerdisplay = document.querySelector(".center");
+
 let URL = "https://api.quotable.io/random";
 
 const getquote = async ()=>{
+    try{
     let respopnse = await fetch(URL);
     let data = await respopnse.json();
-
-    console.log(data);
+    centerdisplay.style.display = "block";
+    quote.textContent = "❝ " + data.content + "  ❞ ";
+    }
+    catch(e){
+        console.error(e);
+    }
 }
 
 mybutton.addEventListener("click", getquote);
