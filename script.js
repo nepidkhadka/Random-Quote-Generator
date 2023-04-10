ef0c4409a6d7986e8366e0b6caaa02663babcd40
Dipen Khadka <dipenkhadka191@gmail.com>
2023-04-10 23:22:53 +0545

    UI Part Completed and JS Part Started

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let url = "https://api.quotable.io/random";
+const mybutton = document.querySelector("button");
+let quote = document.querySelector(".center p")
+let URL = "https://api.quotable.io/random";
 
+const getquote = async ()=>{
+    let respopnse = await fetch(URL);
+    let data = await respopnse.json();
+
+    console.log(data);
+}
+
+mybutton.addEventListener("click", getquote);
