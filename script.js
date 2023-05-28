10dc1c2bfee231d92011ca65d27c91e30dab61ba
Dipen Khadka <dipenkhadka191@gmail.com>
2023-05-28 18:22:53 +0545

    Made Changes To JS & CSS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const mybutton = document.querySelector("button");
 let quote = document.querySelector(".center p");
+let author = document.querySelector(".center h4");
 let centerdisplay = document.querySelector(".center");
 
 let URL = "https://api.quotable.io/random";
@@ -10,6 +11,8 @@ const getquote = async ()=>{
     let data = await respopnse.json();
     centerdisplay.style.display = "block";
     quote.textContent = "❝ " + data.content + "  ❞ ";
+    author.textContent = `—	 ${data.author}`;
+    mybutton.textContent = "Next Quote"
     }
     catch(e){
         console.error(e);
